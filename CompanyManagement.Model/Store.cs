e3e4d00978215aede73eae2652c146d6142688eb
ZeyangSun <sunzeyang0702@gmail.com>
2019-04-12 08:04:12 +0200

    client-side validation

--- CompanyManagement.Model/Store.cs
+++ CompanyManagement.Model/Store.cs
@@ -22,14 +22,17 @@ namespace CompanyManagement.Model
 
         [Required]
         [StringLength(512)]
+        [RegularExpression("^[a-zA-Z\u0080-\u024F]+(?:([\\ \\-\\']|(\\.\\ ))[a-zA-Z\u0080-\u024F]+)*$", ErrorMessage = "City is required and must be properly formatted.")]
         public string City { get; set; }
 
         [Required]
         [StringLength(16)]
+        [RegularExpression("^((\\d{5}-\\d{4})|(\\d{5})|(\\d{4})|(\\d{6})|(\\d{3}\\s\\d{2})|(\\d{2}\\s\\d{3})|([A-Z]\\d[A-Z]\\s\\d[A-Z]\\d))$", ErrorMessage = "Zip code is required and must be properly formatted.")]
         public string Zip { get; set; }
 
         [Required]
         [StringLength(50)]
+        [RegularExpression("^[a-zA-Z\u0080-\u024F]+(?:([\\ \\-\\']|(\\.\\ ))[a-zA-Z\u0080-\u024F]+)*$", ErrorMessage = "Country is required and must be properly formatted.")]
         public string Country { get; set; }
 
         [StringLength(15)]
